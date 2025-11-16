c74a6633164d47ba218dde43c3fc8e742bb8e4c8
Mostafa Hosny <MostafaHosny1016@gmail.com>
2025-11-16 19:41:21 +0200

    AddFrontLink

--- Hospital/Hospital/Program.cs
+++ Hospital/Hospital/Program.cs
@@ -68,7 +68,7 @@ namespace Hospital
             {
                 options.AddPolicy("AllowWebApp",
                     policyBuilder => policyBuilder
-                        .WithOrigins("http://127.0.0.1:5500")
+                        .WithOrigins("http://127.0.0.1:5500", "http://localhost:4200")
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials());
